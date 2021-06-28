bac2634110ee390a0e2d66c61c02e6b91c34002c
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-06-28 16:00:52 -0300

    Reestruturando o Codigo

--- lib/main.dart
+++ lib/main.dart
@@ -22,31 +22,56 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.purple,
       ),
-      home: Scaffold(
-        body: 
-        SizedBox(
-          height: 300,
-          child: Card(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[ 
-                      Image.asset('assets/images/bolo-cenoura.jpeg', fit: BoxFit.fill, height: 280,),
-                      Positioned(
-                        bottom: 10,
-                        left: 10,
-                        child: Text('Bolo de Cenoura', style: TextStyle(fontSize: 25, color: Colors.white),),
-                      )
-                  ],
-                )
-              ],
-          ),
-      )),
-      appBar: AppBar(title: Text('Cozinhando em Casa')),
-      ),
+      home: Home()
     );
   }
 }
 
+class Home extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return _construirHome();
+  }
+
+  Widget _construirHome() {
+    return Scaffold(
+      body: _construirCard(),
+      appBar: _construirAppBar(),
+    );
+  }
+
+  Widget _construirCard() {
+    return SizedBox(
+        height: 300,
+        child: Card(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  _construirImagemCard(),
+                  _construirTextoCard()
+                ],
+              )
+            ],
+          ),
+      ));
+    }
+
+    Widget _construirTextoCard() {
+      
+      return Positioned(
+        bottom: 10,
+        left: 10,
+        child: Text('Bolo de Cenoura', style: TextStyle(fontSize: 25, color: Colors.white)));
+    }
+
+    Widget _construirImagemCard(){
+      return Image.asset('assets/images/bolo-cenoura.jpeg', fit: BoxFit.fill, height: 280,);
+    }
+
+    _construirAppBar() {
+    return AppBar(title: Text('Cozinhando em Casa'));
+  }
+}
